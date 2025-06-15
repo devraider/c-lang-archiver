62f263b1a96d098d00357c96a59c4235221d91e5
crtpaul <cristeapaul.2015@gmail.com>
2025-06-15 23:13:53 +0300

    feat(archiver): validate path is a dir or file

--- src/crtar.c
+++ src/crtar.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-int crtar(char *filename, char *file_path)
+int crtar(char *filename, char *path)
 {
 
-    printf("Creating archive: %s from %s\n", filename, file_path);
+    struct stat path_stat;
+    // check wheter is file or directory
+    stat(path, &path_stat);
+    printf("Creating archive: %s from %s, %s \n", filename, path, path_stat.st_mode & S_IFDIR ? "directory" : "file");
     return 0;
 }
