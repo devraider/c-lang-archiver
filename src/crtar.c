fe5d245a86ab27c890e5c8961d960756d479cb18
crtpaul <cristeapaul.2015@gmail.com>
2025-06-29 19:23:10 +0300

    feat(archiver): added methods to get permission, UID, GUID, path type

--- src/crtar.c
+++ src/crtar.c
@@ -2,22 +2,67 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <string.h>
 
-char *get_path_type(char *path)
+char *get_path_permission(mode_t mode)
 {
-    struct stat path_stat;
-    stat(path, &path_stat);
-    return path_stat.st_mode & S_IFDIR ? "directory" : "file";
+    static char perms[10];
+    perms[0] = S_ISDIR(mode) ? 'd' : '-';
+    perms[1] = (mode & S_IRUSR) ? 'r' : '-';
+    perms[2] = (mode & S_IWUSR) ? 'w' : '-';
+    perms[3] = (mode & S_IXUSR) ? 'x' : '-';
+    perms[4] = (mode & S_IRGRP) ? 'r' : '-';
+    perms[5] = (mode & S_IWGRP) ? 'w' : '-';
+    perms[6] = (mode & S_IXGRP) ? 'x' : '-';
+    perms[7] = (mode & S_IROTH) ? 'r' : '-';
+    perms[8] = (mode & S_IWOTH) ? 'w' : '-';
+    perms[9] = (mode & S_IXOTH) ? 'x' : '-';
+
+    return perms;
+}
+
+int get_path_uid(struct stat path_stat)
+{
+
+    return path_stat.st_uid;
+}
+
+int get_path_guid(struct stat path_stat)
+{
+    return path_stat.st_gid;
+}
+
+// char *get_path_size(mode_t mode_t) {}
+// char *get_path_modified_date(mode_t mode_t) {}
+// char *get_path_checksum(mode_t mode_t) {}
+
+char *get_path_type(struct stat path_stat)
+{
+
+    return path_stat.st_mode &
+                   S_IFDIR
+               ? "directory"
+               : "file";
+}
+
+void file_archiever(char *path)
+{
+    FILE *file = fopen(path, "rb");
 }
 
 int crtar(char *filename, char *path)
 {
+    struct stat path_stat;
+    stat(path, &path_stat);
 
     // check wheter is file or directory
-    char *path_type = get_path_type(path);
+    char *path_type = get_path_type(path_stat);
+    char *path_permission = get_path_permission(path_stat.st_mode);
+    int path_uid = get_path_uid(path_stat);
+    int path_guid = get_path_guid(path_stat);
 
     // TODO: To implement dir logic
-    if (path_type == "directory")
+    if (strcmp(path_type, "directory") == 0)
     {
 
         printf("Brrr.. not implemented directory logic, yet!");
@@ -26,5 +71,9 @@ int crtar(char *filename, char *path)
 
     // read file
     printf("Creating archive: %s from %s (%s) \n", filename, path, path_type);
+    printf(" Path permissions: %s \n", path_permission);
+    printf(" Path User ID: %d \n", path_uid);
+    printf(" Path Group ID: %d \n", path_guid);
+
     return 0;
 }
