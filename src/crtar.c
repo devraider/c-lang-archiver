0b5bb23b61fd2077aabf5a545f01271b4d253f30
crtpaul <cristeapaul.2015@gmail.com>
2025-06-29 16:18:06 +0300

    feat(archiver): return early for dir logic

--- src/crtar.c
+++ src/crtar.c
@@ -3,12 +3,28 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+char *get_path_type(char *path)
+{
+    struct stat path_stat;
+    stat(path, &path_stat);
+    return path_stat.st_mode & S_IFDIR ? "directory" : "file";
+}
+
 int crtar(char *filename, char *path)
 {
 
-    struct stat path_stat;
     // check wheter is file or directory
-    stat(path, &path_stat);
-    printf("Creating archive: %s from %s, %s \n", filename, path, path_stat.st_mode & S_IFDIR ? "directory" : "file");
+    char *path_type = get_path_type(path);
+
+    // TODO: To implement dir logic
+    if (path_type == "directory")
+    {
+
+        printf("Brrr.. not implemented directory logic, yet!");
+        return 1;
+    }
+
+    // read file
+    printf("Creating archive: %s from %s (%s) \n", filename, path, path_type);
     return 0;
 }
